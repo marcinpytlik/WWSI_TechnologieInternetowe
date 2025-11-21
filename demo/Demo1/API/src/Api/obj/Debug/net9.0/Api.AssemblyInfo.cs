730d99386a1cf3ee918d071bc5f5adc27fc26adc
Marcin Pytlik <marcin.pytlik@pracowniait.com>
2025-11-21 07:35:59 +0100

    zm

--- demo/Demo1/API/src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ demo/Demo1/API/src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf57016e7d25092491f7e5f73492680d84aacfa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff6cf409f98a58412cc0b6c17ec721ddc910513c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
