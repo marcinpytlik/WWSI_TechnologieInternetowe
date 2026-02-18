d3805dfb1d20b27ff2896dd72242ad99a5c32fdb
Marcin Pytlik <marcin.pytlik@pracowniait.com>
2026-02-18 11:44:35 +0100

    update repo

--- demo/Demo1/API/src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
+++ demo/Demo1/API/src/Api/obj/Debug/net9.0/Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff6cf409f98a58412cc0b6c17ec721ddc910513c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88b49b1b4f5e3287ece11b25baae260487eb231d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
